dc13f5ace67991cbf59de8d4be605858b31c6810
Omar Mohamed <79914314+OmarMohamedAli@users.noreply.github.com>
2021-04-01 16:28:18 +0200

    Add files via upload

---
+++ Single Dimension Array in c/Array name is Constant Pointer/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Arr[3]={1,2,3};
+    int ptr = NULL;
+    //Arr = ptr ;           // Error
+    //Arr++;               // Error
+    //Arr*2;               // Error
+
+    return 0;
+}
