c2971b7ce037129c332c92f257181a046225a964
Omar Mohamed <79914314+OmarMohamedAli@users.noreply.github.com>
2021-04-01 16:02:38 +0200

    Add files via upload

---
+++ Single Dimension Array in c/Pointer Array Assignment/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A[5];
+    int* ptr;
+    ptr = A;
+
+    // Input elements in array using pointer
+    printf("Enter 5 elements in the array : \n");
+    for (int i=0; i<5; i++)
+    {
+        printf("A[%d] = ",i);
+        scanf("%d",ptr+i);
+    }
+
+    // Print all elements of array using dereference pointer
+    printf("\nFirst Way using *(ptr+i)");
+    printf("\nElements in array are: \n");
+    for (int i=0; i<5; i++)
+    {
+        printf("A[%d] = %d  ",i,*(ptr+i));
+    }
+
+    printf("\n");
+    printf("\nSecond Way using i[A]");
+    printf("\nElements in array are: \n");
+    for (int i=0; i<5; i++)
+    {
+        printf("A[%d] = %d  ",i,i[A]);
+    }
+
+    printf("\n");
+    printf("A[%d] = %d  ",0,(A+1)[-1]);
+    printf("A[%d] = %d  ",1,(A+1)[0]);
+    printf("A[%d] = %d  ",2,(A+1)[1]);
+    printf("A[%d] = %d  ",3,(A+1)[2]);
+    printf("A[%d] = %d  \n",4,(A+1)[3]);
+
+    return 0;
+}
