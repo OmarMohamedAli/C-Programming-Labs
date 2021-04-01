95d21a386b6df711012728abdf6ea75efa4fe3cf
Omar Mohamed <79914314+OmarMohamedAli@users.noreply.github.com>
2021-04-01 12:23:26 +0200

    Add files via upload

---
+++ Single Dimension Array in c/1D-Array Initialization and Print Element Values/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // 1D-Array Declaration
+    char A[4];
+
+  // 1D-Array Initialization
+    A[0]=0;
+    A[1]=2;
+    A[2]=4;
+    A[3]=6;
+
+  // Print 1D-Array
+    printf("First Element in 1D Array is : %d \n",A[0]);
+    printf("Second Element in 1D Array is: %d \n",A[1]);
+    printf("Third Element in 1D Array is : %d \n",A[2]);
+    printf("Fourth Element in 1D Array is: %d \n",A[3]);
+
+    return 0;
+}
