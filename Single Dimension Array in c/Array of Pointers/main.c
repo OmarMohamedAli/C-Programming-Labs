278ae1786beb4fbbb7dab40e5ee866d83856c36d
Omar Mohamed <79914314+OmarMohamedAli@users.noreply.github.com>
2021-04-01 17:49:34 +0200

    Update main.c

--- Single Dimension Array in c/Array of Pointers/main.c
+++ Single Dimension Array in c/Array of Pointers/main.c
@@ -4,14 +4,15 @@
 int main()
 {
 
-    int x, y, z, *Arr0[3];
+    int x=1, y=2, z=3, *Arr0[3];
     Arr0[0] = &x;
     Arr0[1] = &y;
     Arr0[2] = &z;
 
     for (int i; i<3; i++)
     {
-        printf("Arr0[%d] = %d \n",i , Arr0[i]);
+        printf("Arr0[%d] = %d \n",i ,  Arr0[i]);
+        printf("Arr0[%d] = %d \n",i , *Arr0[i]);
     }
 
 
