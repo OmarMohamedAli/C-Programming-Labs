3f9f31993bdd148098e6c0b1fc652a15c0899c4b
Omar Mohamed <79914314+OmarMohamedAli@users.noreply.github.com>
2021-04-01 12:13:09 +0200

    Add files via upload

---
+++ Single Dimension Array in c/1D-Array Declaration/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char A[4];  // Array Size is necessary in Declaration
+ // char B[];   // error: array size missing in 'B'
+
+    return 0;
+}
